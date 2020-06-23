0abfb579decdf6667d6c2c8111754e3e2089d797
Anthony Bratcher <tonybrat74@gmail.com>
2020-06-22 19:40:30 -0700

    componentscreated

---
+++ src/Components/Picture.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Picture =  () => {
+    return(
+        <h3>I am the child</h3>
+       /* <div>
+           <img className = "img"
+           src={image}
+           alt= "Nasa Photo of the Day"/>
+           </div>*/
+    )
+}
+
+
+
+
+
+
+
+
+export default Picture
---
+++ src/Components/PictureFrame.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect } from "react";
+import axios from "axios";
+import Picture from "./Picture"
+
+
+const PictureFrame =  () => {
+    return(
+        <div>
+           <h1>this is the Parent</h1>
+           <Picture/>
+           </div>
+    )
+}
+
+
+
+
+
+
+
+
+export default PictureFrame
